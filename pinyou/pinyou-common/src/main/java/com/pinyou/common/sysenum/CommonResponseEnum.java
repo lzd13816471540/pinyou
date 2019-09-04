06fbfcece81bd7c61eec4f544991e5c839e301a3
lizhidong <13816471540@163.com>
2019-09-04 21:52:58 +0800

    提交代码

--- pinyou/pinyou-common/src/main/java/com/pinyou/common/sysenum/CommonResponseEnum.java
+++ pinyou/pinyou-common/src/main/java/com/pinyou/common/sysenum/CommonResponseEnum.java
@@ -8,22 +8,22 @@ package com.pinyou.common.sysenum;
  */
 public enum CommonResponseEnum {
 
-    SUCCESS("200","成功"),
-    ;
+    SUCCESS("200", "成功");
 
     private String code;
 
     private String msg;
 
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
     CommonResponseEnum(String code, String msg) {
         this.code = code;
         this.msg = msg;
     }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getMsg() {
-        return msg;
-    }}
+}
