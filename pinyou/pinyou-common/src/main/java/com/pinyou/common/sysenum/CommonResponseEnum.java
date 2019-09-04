cb42b8a6a71b112fd09ec0bc989c22cf09cee1d1
lizhidong <13816471540@163.com>
2019-09-04 22:26:27 +0800

    提交代码

--- pinyou/pinyou-common/src/main/java/com/pinyou/common/sysenum/CommonResponseEnum.java
+++ pinyou/pinyou-common/src/main/java/com/pinyou/common/sysenum/CommonResponseEnum.java
@@ -1,11 +1,16 @@
 package com.pinyou.common.sysenum;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @ClassName CommonResponseEnum
  * @Discription TODO
  * @Author zhidong.li
  * @Date 2019年08月16日 10:05
  */
+@Getter
+@AllArgsConstructor
 public enum CommonResponseEnum {
 
     SUCCESS("200", "成功");
@@ -14,16 +19,4 @@ public enum CommonResponseEnum {
 
     private String msg;
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setMsg(String msg) {
-        this.msg = msg;
-    }
-
-    CommonResponseEnum(String code, String msg) {
-        this.code = code;
-        this.msg = msg;
-    }
 }
